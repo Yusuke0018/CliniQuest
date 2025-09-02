7b2f180b5b258d37efdd7783945c3ce39f70f881
総合クリニック大阪1診 <osakasoshin1@sougoukurinikkuoosaka1shinnoiMac.local>
2025-09-02 16:40:31 +0900

    chore: track config.js (public client config) and fix storageBucket domain
    config.js を .gitignore から除外。storageBucket を <projectId>.appspot.com に補正。

---
+++ config.js
@@ -0,0 +1,11 @@
+// Firebase Web Config（公開可のクライアント設定）
+// ゆうすけ様のプロジェクト値を反映しています。
+// 備考: storageBucket はバケット名（通常は <projectId>.appspot.com）を指定します。
+window.CLQ_FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyDy9D15rMzWe9v3CR74PT79ftQge2xxmPA',
+  authDomain: 'cliniquest-8b6fd.firebaseapp.com',
+  projectId: 'cliniquest-8b6fd',
+  storageBucket: 'cliniquest-8b6fd.appspot.com',
+  messagingSenderId: '833667687672',
+  appId: '1:833667687672:web:93658052179813a038c70d',
+};
